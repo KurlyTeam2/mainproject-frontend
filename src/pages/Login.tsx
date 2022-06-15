eff070f24ee04f86e6378b81dfbbfc7311b6d191
yoochan0322 <yoochan0322@naver.com>
2022-06-15 16:07:19 +0900

    feat: 로그인 ui 구현

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,109 @@
 import React from 'react';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import InputLabel from '@mui/material/InputLabel';
+import InputAdornment from '@mui/material/InputAdornment';
+import FormControl from '@mui/material/FormControl';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
 
-export default function Home() {
+
+interface State {
+    amount: string;
+    password: string;
+    weight: string;
+    weightRange: string;
+    showPassword: boolean;
+  }
+
+function Login() {
+
+    const [values, setValues] = React.useState<State>({
+        amount: '',
+        password: '',
+        weight: '',
+        weightRange: '',
+        showPassword: false,
+    });
+
+    const handleChange =
+    (prop: keyof State) => (event: React.ChangeEvent<HTMLInputElement>) => {
+        setValues({ ...values, [prop]: event.target.value });
+    };
+
+    const handleClickShowPassword = () => {
+        setValues({
+            ...values,
+            showPassword: !values.showPassword,
+        });
+    };
+
+    const handleMouseDownPassword = (event: React.MouseEvent<HTMLButtonElement>) => {
+        event.preventDefault();
+    };
+
+
     return (
-        <div>로그인입니다</div>
+        <Box 
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+            minHeight="100vh">
+            <Box
+                sx={{
+                    bgcolor: 'background.paper',
+                    boxShadow: 1,
+                    borderRadius: 2,
+                    p: 10,
+                    minWidth: 300,
+                    maxWidth: 600,
+                    flexWrap: 'wrap' 
+                }}>
+                
+                <Stack spacing={2} direction="column">
+                    <TextField
+                        id="user_id"
+                        label="ID"/>
+                    <FormControl sx={{ width: '100%'}} variant="outlined">
+                        <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
+                        <OutlinedInput
+                            required
+                            id="user_password"
+                            type={values.showPassword ? 'text' : 'password'}
+                            value={values.password}
+                            onChange={handleChange('password')}
+                            endAdornment={
+                                <InputAdornment position="end">
+                                    <IconButton
+                                        aria-label="toggle password visibility"
+                                        onClick={handleClickShowPassword}
+                                        onMouseDown={handleMouseDownPassword}
+                                        edge="end"
+                                    >
+                                        {values.showPassword ? <VisibilityOff /> : <Visibility />}
+                                    </IconButton>
+                                </InputAdornment>
+                            }
+                            label="Password"
+                        />
+                    </FormControl>
+
+                    <Stack direction="row-reverse">
+                        <Button variant="contained" size="medium" color="secondary">로그인</Button>
+                    </Stack>
+
+
+                    
+                </Stack>
+            </Box>
+            
+        </Box>
+        
+        
     );
 }
+export default Login;
