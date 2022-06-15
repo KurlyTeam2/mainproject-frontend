5a958d397f900d68f67bbf233e80719763f42897
GYUMINSHIN <92241822+GYUMINSHIN@users.noreply.github.com>
2022-06-15 13:55:27 +0900

    feat: 페이지 이동 구현
    리액트 라우터로 페이지 이동 구현

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
+import {BrowserRouter} from 'react-router-dom';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
 import App from './App';
 
-ReactDOM.render(
+root.render(
     <React.StrictMode>
-        <App />
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
     </React.StrictMode>,
-    document.getElementById('root'),
 );
 
 // If you want to start measuring performance in your app, pass a function
